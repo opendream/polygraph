a9fcced24e6690d0fb88a70e2095384084b17f80
crosalot <crosalot@gmail.com>
2014-05-22 11:46:36 +0700

    add statement page

--- domain/urls.py
+++ domain/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('domain.views',
     url(r'^$', 'domain_home', name='domain_home'),
+    url(r'^statement/list/$', 'domain_statement_list', name='domain_statement_list'),
+    url(r'^statement/detail/(?P<statement_permalink>[A-Za-z0-9-_.]+)/$', 'domain_statement_detail', name='domain_statement_detail'),
+
 )
--- domain/views.py
+++ domain/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 def domain_home(request):
 
-    return render(request, 'domain/domain_home.html', {})
+    return render(request, 'domain/home.html', {})
+
+
+def domain_statement_list(request):
+
+    return render(request, 'domain/statement_list.html', {})
+
+def domain_statement_detail(request, statement_permalink):
+
+    return render(request, 'domain/statement_detail.html', {statement_permalink: statement_permalink})
