a432ca0fd26c93e3e8a186cb4f4842f045236be7
crosalot <crosalot@gmail.com>
2014-05-26 18:35:32 +0700

    ref edit and create people

--- domain/tests/test_view.py
+++ domain/tests/test_view.py
@@ -4,33 +4,42 @@ from django.utils.translation import ugettext_lazy as _
 from django.test import TestCase
 
 from common import factory
+from domain.models import People
+
 
 class TestEditPeople(TestCase):
 
     def setUp(self):
 
         self.staff1 = factory.create_staff('staff', 'crosalot@kmail.com', 'password', ' Crosalot', 'Opendream ', 'Developer', 'Opensource', 'http://opendream.co.th')
-        self.people1 = factory.create_people('crosalot',' Crosalot', 'Opendream ', 'Developer', 'Opensource', 'http://opendream.co.th')
         self.people2 = factory.create_people('panudate',' Panudate', 'Vasinwattana', 'Tester', 'Unittest', 'http://opendream.in.th')
 
         self.client.login(username=self.staff1.username, password='password')
+
+        # Define for override
+        self.check_initial = True
+        self.people1 = factory.create_people('crosalot',' Crosalot', 'Opendream ', 'Developer', 'Opensource', 'http://opendream.co.th')
+        self.url1 = reverse('people_edit', args=[self.people1.id])
+        self.url2 = reverse('people_edit', args=[self.people2.id])
+        self.message_success = _('Your settings has been updated.')
+
 
 
     def test_get_edit_people_page(self):
 
         self.client.logout()
 
-        response = self.client.get(reverse('people_edit', args=[self.people1.id]))
-        self.assertRedirects(response, '%s?next=%s' % (reverse('account_login'), reverse('people_edit', args=[self.people1.id])))
+        response = self.client.get(self.url1)
+        self.assertRedirects(response, '%s?next=%s' % (reverse('account_login'), self.url1))
 
         self.client.login(username=self.staff1.username, password='password')
-        response = self.client.get(reverse('people_edit', args=[self.people1.id]))
+        response = self.client.get(self.url1)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'domain/people_edit.html')
+        self.assertTemplateUsed(response, 'domain/people_form.html')
         self.client.logout()
 
     def test_edit_people_context(self):
-        response = self.client.get(reverse('people_edit', args=[self.people1.id]))
+        response = self.client.get(self.url1)
 
         self.assertContains(response, 'name="permalink"')
         self.assertContains(response, 'name="first_name"')
@@ -38,6 +47,9 @@ class TestEditPeople(TestCase):
         self.assertContains(response, 'name="occupation"')
         self.assertContains(response, 'name="description"')
         self.assertContains(response, 'name="homepage_url"')
+
+        if not self.check_initial:
+            return
 
         self.assertContains(response, self.people1.permalink)
         self.assertContains(response, self.people1.first_name)
@@ -47,7 +59,7 @@ class TestEditPeople(TestCase):
         self.assertContains(response, self.people1.homepage_url)
 
 
-        response = self.client.get(reverse('people_edit', args=[self.people2.id]))
+        response = self.client.get(self.url2)
         self.assertContains(response, self.people2.permalink)
         self.assertContains(response, self.people2.first_name)
         self.assertContains(response, self.people2.last_name)
@@ -55,8 +67,37 @@ class TestEditPeople(TestCase):
         self.assertContains(response, self.people2.description)
         self.assertContains(response, self.people2.homepage_url)
 
+    def test_edit_people_post(self):
+
+        params = {
+            'permalink': self.people1.permalink,
+            'first_name': self.people1.first_name,
+            'last_name': self.people1.last_name,
+            'occupation': self.people1.occupation,
+            'description': self.people1.description,
+            'homepage_url': self.people1.homepage_url,
+        }
+
+        response = self.client.post(self.url1, params, follow=True)
+
+
+        self.assertContains(response, self.people1.permalink)
+        self.assertContains(response, self.people1.first_name)
+        self.assertContains(response, self.people1.last_name)
+        self.assertContains(response, self.people1.occupation)
+        self.assertContains(response, self.people1.description)
+        self.assertContains(response, self.people1.homepage_url)
+        self.assertContains(response, self.message_success)
+
+    def test_has_new(self):
+        before = People.objects.all().count()
+        self.test_edit_people_post()
+        after = People.objects.all().count()
+
+        self.assertEqual(int(not self.check_initial), after-before)
+
+
     def test_post_edit_people_invalid(self):
-        self.client.login(username=self.staff1.email, password='password')
 
         params = {
             'permalink': '',
@@ -66,7 +107,7 @@ class TestEditPeople(TestCase):
             'description': '',
             'homepage_url': '',
         }
-        response = self.client.post(reverse('people_edit', args=[self.people1.id]), params)
+        response = self.client.post(self.url1, params)
         self.assertFormError(response, 'form', 'permalink', [_('This field is required.')])
         self.assertFormError(response, 'form', 'first_name', [_('This field is required.')])
         self.assertFormError(response, 'form', 'last_name', [_('This field is required.')])
@@ -80,13 +121,12 @@ class TestEditPeople(TestCase):
             'description': '',
             'homepage_url': '',
         }
-        response = self.client.post(reverse('people_edit', args=[self.people1.id]), params)
+        response = self.client.post(self.url1, params)
         self.assertFormError(response, 'form', 'permalink',  [_('This permalink is already in use.')])
 
         self.client.logout()
 
     def test_post_edit_profile_not_update(self):
-        self.client.login(username=self.staff1.email, password='password')
 
         params = {
             'permalink': self.people1.permalink,
@@ -94,5 +134,27 @@ class TestEditPeople(TestCase):
             'last_name': self.people1.last_name,
         }
 
-        response = self.client.post(reverse('people_edit', args=[self.people1.id]), params, follow=True)
-        self.assertContains(response, _('Your settings has been updated.'))
+        response = self.client.post(self.url1, params, follow=True)
+        self.assertContains(response, self.message_success)
+
+
+class TestCreatePeople(TestEditPeople):
+
+    def setUp(self):
+        super(TestCreatePeople, self).setUp()
+
+        self.check_initial = False
+
+
+        self.people1 = People(**{
+            'permalink': 'new-crosalot',
+            'first_name': 'New',
+            'last_name': 'Crosalot',
+            'occupation': 'Designer',
+            'description': 'Work on opendream',
+            'homepage_url': 'http://opendream.co.th',
+        })
+
+        self.url1 = reverse('people_create')
+        self.url2 = reverse('people_create')
+        self.message_success = _('New %s has been created. View this %s <a href="%s">here</a>.') % (_('people'), _('people'), '#')
--- domain/urls.py
+++ domain/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('domain.views',
     url(r'^statement/detail/(?P<statement_permalink>[A-Za-z0-9-_.]+)/$', 'statement_detail', name='statement_detail'),
 
     url(r'^people/edit/(?P<people_id>\d+)/$', 'people_edit', name='people_edit'),
+    url(r'^people/create/$', 'people_create', name='people_create'),
+
 
 )
