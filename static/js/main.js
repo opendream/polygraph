e6f538d39afbef8ea46c6dced837ab732e596209
crosalot <crosalot@gmail.com>
2014-06-09 15:55:19 +0700

    complete add reference

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,51 @@
+function cloneMore(selector, type) {
+
+    var newElement = $(selector).clone(true);
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        var id = 'id_' + name;
+        $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+    });
+    newElement.find('label').each(function() {
+        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+        $(this).attr('for', newFor);
+    });
+    total++;
+    $('#id_' + type + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+}
+
 $(document).ready(function () {
     $('input[type=checkbox], input[type=radio]').each(function () {
         $(this).prettyCheckable();
     });
+
+    $('.add_more').click(function() {
+
+
+        var selector = '#' + $(this).attr('id').replace('_add_more', '');
+        var type = selector.replace('#id_', '')
+
+        selector = selector + '>:last'
+
+        var newElement = $(selector).clone(true);
+        var total = $('#id_' + type + '-TOTAL_FORMS').val();
+        newElement.find(':input').each(function() {
+            var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+            var id = 'id_' + name;
+            $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+        });
+        newElement.find('label').each(function() {
+            var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+            $(this).attr('for', newFor);
+        });
+        total++;
+        $('#id_' + type + '-TOTAL_FORMS').val(total);
+        $(selector).after(newElement);
+
+    });
+
 });
 
 if (typeof CKEDITOR != 'undefined') {
