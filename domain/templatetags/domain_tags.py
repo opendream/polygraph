5cba29b84e19ad230203b06407d60bf051b429ea
crosalot <crosalot@gmail.com>
2014-07-07 10:22:57 +0700

    add meter_count template tags

--- domain/templatetags/domain_tags.py
+++ domain/templatetags/domain_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.filter
 def statement_item(statement, style='normal'):
     return render_to_string('domain/statement_item_%s.html' % style, {'statement': statement})
+
+@register.filter
+def meter_count(meter_statement_count):
+    return render_to_string('domain/meter_count.html', {'meter_statement_count': meter_statement_count})
