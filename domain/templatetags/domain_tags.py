392800a7b535d97e651879e48759125833f6d67c
crosalot <crosalot@gmail.com>
2014-07-10 10:32:46 +0700

    add percent bar

--- domain/templatetags/domain_tags.py
+++ domain/templatetags/domain_tags.py
@@ -10,4 +10,22 @@ def statement_item(statement, style='normal'):
 
 @register.filter
 def meter_count(meter_statement_count):
-    return render_to_string('domain/meter_count.html', {'meter_statement_count': meter_statement_count})
+
+    total = 0
+    for meter, count in meter_statement_count:
+        if count > total:
+            total = count
+
+    meter_statement_count_percent = []
+    for meter, count in meter_statement_count:
+        if total == 0:
+            percent = 100
+        else:
+            percent = count*100/total
+        meter_statement_count_percent.append((meter, count, percent))
+
+
+    return render_to_string('domain/meter_count.html', {
+        'meter_statement_count': meter_statement_count_percent
+    })
+
