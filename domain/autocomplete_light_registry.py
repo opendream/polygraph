cf6d4024fbf53d53b85897a6482b0f85534e2b65
crosalot <root@admin@od.dev>
2014-07-24 15:35:32 +0700

    fixed miss autocomplete

--- domain/autocomplete_light_registry.py
+++ domain/autocomplete_light_registry.py
@@ -23,19 +23,46 @@ class PeopleAutocomplete(autocomplete_light.AutocompleteModelBase):
             self.choice_label(choice))
 
 
-def get_default_topic_quesrset():
+def get_default_topic_queryset():
 
     query = TopicRevision.objects.all().order_by('-id').query
     query.group_by = ['origin_id']
     results = QuerySet(query=query, model=TopicRevision)
+
 
     return results
 
 
 class TopicAutocomplete(autocomplete_light.AutocompleteModelBase):
 
-    choices = get_default_topic_quesrset()
+    choices = get_default_topic_queryset()
     search_fields = ['title']
+
+    def choices_for_values(self):
+        """
+        Return ordered choices which pk are in
+        :py:attr:`~.base.AutocompleteInterface.values`.
+        """
+        assert self.choices is not None, 'choices should be a queryset'
+        return self.order_choices(self.choices.filter(
+            origin__id__in=self.values or []))
+
+    def choices_for_request(self):
+        """
+        Return a queryset based on :py:attr:`choices` using options
+        :py:attr:`split_words`, :py:attr:`search_fields` and
+        :py:attr:`limit_choices`.
+        """
+        assert self.choices is not None, 'choices should be a queryset'
+        assert self.search_fields, 'autocomplete.search_fields must be set'
+        q = self.request.GET.get('q', '')
+        exclude = self.request.GET.getlist('exclude')
+
+        conditions = self._choices_for_request_conditions(q,
+                self.search_fields)
+
+        return self.order_choices(self.choices.filter(
+            conditions).exclude(origin__id__in=exclude))[0:self.limit_choices]
 
     def choice_value(self, choice):
 
@@ -52,9 +79,17 @@ class TopicAutocomplete(autocomplete_light.AutocompleteModelBase):
             self.choice_label(choice))
 
 
+def get_default_statement_queryset():
+
+    query = Statement.objects.all().order_by('-id').query
+    query.group_by = ['id']
+    results = QuerySet(query=query, model=Statement)
+
+    return results
+
 class StatementAutocomplete(autocomplete_light.AutocompleteModelBase):
 
-    choices = Statement.objects.all().order_by('-id')
+    choices = get_default_statement_queryset()
     search_fields = ['quote', 'permalink', 'topic__topicrevision__title']
 
     def choice_label(self, choice):
