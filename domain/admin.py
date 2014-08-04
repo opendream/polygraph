7d202325c1d72b022ea61ac9d6d87d3f5f933e2a
crosalot <crosalot@gmail.com>
2014-08-04 14:24:53 +0700

    make admin safe 2

--- domain/admin.py
+++ domain/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from common.functions import meter_render_reference
 
 from domain.models import PeopleCategory, People, Topic, TopicRevision, Meter, Statement
 
@@ -10,7 +11,13 @@ class TopicAdmin(admin.ModelAdmin):
     inlines = [ TopicRevisionInline, ]
 
 class MeterAdmin(admin.ModelAdmin):
-    list_display = ('image_small', 'title')
+
+    list_display = ('rendered', )
+
+    def rendered(self, obj):
+        return meter_render_reference(obj)
+
+    rendered.allow_tags = True
 
 admin.site.register(PeopleCategory)
 admin.site.register(People)
