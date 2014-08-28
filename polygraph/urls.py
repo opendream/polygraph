92a619bb6a11fe500ecfb34fee71ce7a8bbc9809
crosalot <crosalot@gmail.com>
2014-08-28 15:42:21 +0700

    add robots.txt

--- polygraph/urls.py
+++ polygraph/urls.py
@@ -2,6 +2,7 @@ import os
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 admin.autodiscover()
@@ -38,4 +39,10 @@ if settings.DEBUG:
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
+        url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+
+    )
+else:
+    urlpatterns += patterns('',
+        url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
     )
