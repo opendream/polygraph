1ca09e195d53307e3399002c61ba2e16aa55e7f0
crosalot <crosalot@elves-od.local>
2014-09-08 18:20:54 +0700

    fixed people on meter detail

--- domain/views.py
+++ domain/views.py
@@ -327,7 +327,7 @@ def meter_detail(request, meter_permalink=None):
     people_list = people_list.exclude(id__in=people_statement_list)
 
     people_limit = min(3, len(statement_list))
-    people_limit = people_limit - (people_limit % 2)
+    people_limit = people_limit
     people_list = people_list[0:people_limit]
 
 
