2f1f8721df21fa6a83edea73de6e84b79f209621
Nattapol Kengkuntod <Dew@Nattapols-MacBook-Pro.local>
2014-09-18 16:41:57 +0700

    try to edit 404.html

--- polygraph/urls.py
+++ polygraph/urls.py
@@ -40,7 +40,10 @@ if settings.DEBUG:
             'document_root': settings.MEDIA_ROOT,
         }),
         url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+        url(r'^404$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500$', TemplateView.as_view(template_name='500.html')),
     )
+    
 
     import debug_toolbar
     urlpatterns += patterns('',
