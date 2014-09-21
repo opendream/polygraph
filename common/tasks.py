40be83773a663605f81132a3f184f206d713b0cc
crosalot <crosalot#gmail.com>
2014-09-21 17:38:30 +0700

    add super warm

--- common/tasks.py
+++ common/tasks.py
@@ -7,7 +7,10 @@ def warm_cache():
 
     from django.test.client import Client
     from django.core.urlresolvers import reverse
-    from domain.models import Meter
+    from domain.models import Meter, Statement, People, PeopleCategory
+    from domain.views import statement_query_base, people_query_base
+    from common.constants import STATUS_DRAFT, STATUS_PENDING
+
 
     client = Client()
     client.get(reverse('home'))
@@ -17,3 +20,15 @@ def warm_cache():
 
     for meter in Meter.objects.all():
         client.get(reverse('meter_detail', args=[meter.permalink]))
+
+    # TODO: Flag extra warm when only cron run
+    if True:
+
+        for category in PeopleCategory.objects.all():
+            client.get(reverse('people_list'), {'category': category.permalink})
+
+        for statement in Statement.objects.exclude(status__in=[STATUS_DRAFT, STATUS_PENDING]).order_by('-hilight', '-promote', '-changed', '-created')[0:20]:
+            client.get(reverse('statement_detail', args=[statement.permalink]))
+
+        for people in People.objects.exclude(status__in=[STATUS_DRAFT, STATUS_PENDING]).order_by('-quoted_by__created')[0:20]:
+            client.get(reverse('people_detail', args=[people.permalink]))
