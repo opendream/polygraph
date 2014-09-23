d1786f2f0c531a86715e866e7484d07515b4471a
crosalot <crosalot#gmail.com>
2014-09-23 17:53:45 +0700

    add url statement item

--- domain/urls.py
+++ domain/urls.py
@@ -27,6 +27,7 @@ urlpatterns = patterns('domain.views',
     url(r'^statement/(?P<statement_permalink>[A-Za-z0-9-_.]+)/topic-revision/(?P<topicrevision_id>\d+)/$', 'statement_topicrevision_detail', name='statement_topicrevision_detail'),
     url(r'^statement/$', 'statement_list', name='statement_list'),
     url(r'^statement/tags/(?P<tags_id>\d+)/$', 'statement_tags_detail', name='statement_tags_detail'),
+    url(r'^statement/(?P<statement_id>\d+)/item/$', 'statement_item', name='statement_item'),
 
     # Writer
     url(r'^manage/$', 'manage', name='manage'),
